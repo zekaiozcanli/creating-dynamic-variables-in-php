6e42c6ac31126db7493e0738bd713bd9fbbeab1d
Zekai ÖZCANLI <33030152+zekaiozcanli@users.noreply.github.com>
2022-09-19 22:36:42 +0300

    Update view.php

--- view.php
+++ view.php
@@ -1,15 +1,22 @@
 <?php
 
+// We will find the payment total for 12 months
+// And for this we are using for loop (12 Times)
 for($j = 1; $j<=12; $j++)
 {
-	if(strlen($j)<2){ $m = "0".$j; }else{ $m = $j; }
-	${'sql'.$j} = $db->prepare("SELECT  SUM(paid) AS totalDues  FROM  $tblA  WHERE  date>='2020-$m-01'  AND  date<='2020-$m-31' ");
+	
+	//If the j value is a single digit, we add 0 to the beginning so that the j value corresponds to the month value in the database query
+	if(strlen($j)<2)	 
+		$m = "0".$j; 	
+	else	 
+		$m = $j; 
+	
+	//We name the sql variable that we transfer the query result to as sql1, sql2...sql12
+	${'sql'.$j} = $db->prepare("SELECT  SUM(paid) AS totalDues  FROM  db_tablename  WHERE  date>='2020-$m-01'  AND  date<='2020-$m-31' ");
 
 	${'sql'.$j}->execute();
 	${'RA'.$j} = ${'sql'.$j}->fetch(PDO::FETCH_ASSOC);
 	${'td'.$j} = ${'RA'.$j}["totalDues"];
-	if(empty(${'td'.$j})){${'td'.$j}=0;}
-	if($j!=12){ $total_due_sequence .= ${'td'.$j}.","; }else{ $total_due_sequence .= ${'td'.$j}; }
 }
 
 ?>
